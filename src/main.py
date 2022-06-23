a76b76ef1f486e3033c06bf4caabc0563f7a1f0b
planetjunkie <alshhh.123.987@gmail.com>
2022-06-23 14:57:25 +0400

    Buttons and changes :D

--- src/main.py
+++ src/main.py
@@ -5,27 +5,42 @@ wintitle = "PowerMenu QT"
 
 def shutdowncmd():
   os.system("shutdown -h now")
+
 def rebootcmd():
   os.system("reboot")
 
+def logoutcmd():
+  os.system("kill -9 -1")
+
+buttonwidth = 150
+buttonheight = 100
 
 def main():
   application = QApplication([])
   window = QWidget()
   window.setWindowTitle(wintitle)
+  window.setGeometry(200, 200, 200, 200)
   window.show()
 
   shutdownbtn = QPushButton("Shutdown")
   rebootbtn = QPushButton("Reboot")
+  logoutbtn = QPushButton("Logout")
+
+  shutdownbtn.setFixedSize(buttonwidth, buttonheight)
+  rebootbtn.setFixedSize(buttonwidth, buttonheight)
+  logoutbtn.setFixedSize(buttonwidth, buttonheight)
   
+
   shutdownbtn.clicked.connect(shutdowncmd)
   rebootbtn.clicked.connect(rebootcmd)
-
+  logoutbtn.clicked.connect(logoutcmd)
+  
   
 
   layout = QHBoxLayout()
   layout.addWidget(shutdownbtn)
   layout.addWidget(rebootbtn)
+  layout.addWidget(logoutbtn)
   window.setLayout(layout)
 
   application.exec()
