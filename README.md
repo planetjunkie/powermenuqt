66101022f54aa5c6fbffd26ecdeef5c849cb240d
planetjunkie <alshhh.123.987@gmail.com>
2022-06-27 21:28:35 +0430

    Added PyQt5 Version

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 ### Powermenu QT
 
 I made this cuz i have nothing better to do
+
+### Before you use
+Check the Stuff folder and do cd to it, afterwards install the requirements.txt
---
+++ Stuff/main (pyqt5).py
@@ -0,0 +1,52 @@
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import  QSize
+import os
+wintitle = "PowerMenu QT"
+
+
+def shutdowncmd():
+  os.system("shutdown -h now")
+
+def rebootcmd():
+  os.system("reboot")
+
+def logoutcmd():
+  os.system("kill -9 -1")
+
+buttonwidth = 150
+buttonheight = 100
+
+def main():
+  application = QApplication([])
+  window = QWidget()
+  window.setWindowTitle(wintitle)
+  window.setFixedSize(QSize(500, 200))
+  window.show()
+
+  shutdownbtn = QPushButton("Shutdown")
+  rebootbtn = QPushButton("Reboot")
+  logoutbtn = QPushButton("Logout")
+
+  shutdownbtn.setFixedSize(buttonwidth, buttonheight)
+  rebootbtn.setFixedSize(buttonwidth, buttonheight)
+  logoutbtn.setFixedSize(buttonwidth, buttonheight)
+  
+
+  shutdownbtn.clicked.connect(shutdowncmd)
+  rebootbtn.clicked.connect(rebootcmd)
+  logoutbtn.clicked.connect(logoutcmd)
+  
+  layout = QHBoxLayout()
+
+
+  
+  layout.addWidget(shutdownbtn)
+  layout.addWidget(rebootbtn)
+  layout.addWidget(logoutbtn)
+
+  window.setLayout(layout)
+
+  application.exec()
+
+if __name__ == '__main__':
+  main()
